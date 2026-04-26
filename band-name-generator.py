c139a0fa909c5c9596318aa4b3af5a2c3afcf540
Jeevika Godachi <jeevikagodachi04@gmail.com>
2026-04-26 21:35:43 +0530

    Added band name generator program in Python

---
+++ band-name-generator.py
@@ -0,0 +1,4 @@
+print("welcome To Band name")
+Grew_name =input("Enter  name of city ,you have been grew up:\n")
+pet_name =input("Enter the name of your lovely pet :\n")
+print("Your Band name  Could be :" ,Grew_name+ pet_name)
